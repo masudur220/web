7ee51ff0b331e0c1fe74477955c272bf145d4286
masudur220 <75464344+masudur220@users.noreply.github.com>
2021-07-09 00:05:15 +0600

    8787
    uyuy

---
+++ js/homeMain.js
@@ -0,0 +1,139 @@
+
+var a1 = 'https://drive.google.com/file/d/1omNyuP5wivTvHN8VujAUsOjKXSLL2-t6/view?usp=sharing';
+
+var a2 = 'https://drive.google.com/file/d/1XaLox9dOvJqcBZWtydocWsErfpX2pVia/view?usp=sharing';
+
+var a3 = 'https://drive.google.com/file/d/1orZWzOU3uOUSTHtre6TbEjT_CWQ66EJ-/view?usp=sharing';
+
+var a4 = 'https://drive.google.com/file/d/1-vEu0RXOTY4rT1Hqimb-nKejfUg5nymK/view?usp=sharing';
+
+var a5 = 'https://drive.google.com/file/d/1cBWkomH07vjlH7k01pYJ_8bk1SMee-HF/view?usp=sharing';
+
+var a6 = 'https://drive.google.com/file/d/1Vaslq_ReSIzwlbCaxvsRv_NCDgu8rfjE/view?usp=sharing';
+
+var a7 = 'https://drive.google.com/file/d/1l_Aka31cvCqcCGuqkoHk-EjoUPOcrg2l/view?usp=sharing';
+
+var a8 = 'https://drive.google.com/file/d/1awaGurtzkZcA4tbTjhoCpjYJXh0D9YZI/view?usp=sharing';
+
+
+var b1 = 'https://drive.google.com/file/d/1ee1dQ4UUl6GYieThQD7QYqVxp-FWPDdO/view?usp=sharing';
+
+var b2 = 'https://drive.google.com/file/d/1oikc6tWp0bl66pTPe4lQ33LLrWq-n1TM/view?usp=sharing';
+
+var b3 = 'https://drive.google.com/file/d/1y0GRlIzlgL0PEiFmxRtqOHTBV3HZKf2S/view?usp=sharing';
+
+var b4 = 'https://drive.google.com/file/d/16DHNpnjSfz6T2o2vlACe-x9HpRjw5zzn/view?usp=sharing';
+
+
+var c1 = 'https://drive.google.com/file/d/1GpVE4CvkHyywUayqLt8cPr13MgYH-6UN/view?usp=sharing';
+
+var c2 = 'https://drive.google.com/file/d/145Let8pENAItvRB-rX1mHhIpq5ij5na6/view?usp=sharing';
+
+var c3 = 'https://drive.google.com/file/d/1nMHlVb4ZLdotT_WfnjmG0TL6gcdV84aW/view?usp=sharing';
+
+var c4 = 'https://drive.google.com/file/d/1dEULb7wNRICL0G-NxU44Npbu1wq2h8q8/view?usp=sharing';
+
+
+var d1 = 'https://drive.google.com/file/d/1ykWl4nptGOSl_7En6TjSdZmMW2zsnnVN/view?usp=sharing';
+
+var d2 = 'https://drive.google.com/file/d/1WXdFykbBpYJFHVqsAQKUHnAnu65-TNy_/view?usp=sharing';
+
+var d3 = 'https://drive.google.com/file/d/1qQn0OcCe84sErefIpYIp4k900Ny4AJcs/view?usp=sharing';
+
+var d4 = 'https://drive.google.com/file/d/1849wC8rwLgYAzpNxa_zWdNMx4Ik1kKgl/view?usp=sharing';
+
+function home(x){
+    if (x == 1) {
+        window.open(a1);
+    } else if (x == 2){
+        window.open(a2);
+    } else if (x == 3){
+        window.open(a3);
+    } else if (x == 4){
+        window.open(a4);
+    } else if (x == 5){
+        window.open(a5);
+    } else if (x == 6){
+        window.open(a6);
+    } else if (x == 7){
+        window.open(a7);
+    } else if (x == 8){
+        window.open(a8);
+    } else if (x == 9){
+        window.open(b1);
+    } else if (x == 10){
+        window.open(b2);
+    } else if (x == 11){
+        window.open(b3);
+    } else if (x == 12){
+        window.open(b4);
+    } else if (x == 13){
+        window.open(c1);
+    } else if (x == 14){
+        window.open(c2);
+    } else if (x == 15){
+        window.open(c3);
+    } else if (x == 16){
+        window.open(c4);
+    } else if (x == 17){
+        window.open(d1);
+    } else if (x == 18){
+        window.open(d2);
+    } else if (x == 19){
+        window.open(d3);
+    } else if (x == 20){
+        window.open(d4);
+    } else if (x == 21){
+        window.open('');
+    } else if (x == 22){
+        window.open('');
+    } else if (x == 23){
+        window.open('');
+    } else if (x == 24){
+        window.open('');
+    } else if (x == 25){
+        window.open('');
+    } else if (x == 26){
+        window.open('');
+    } else if (x == 27){
+        window.open('');
+    } else if (x == 28){
+        window.open('');
+    } else if (x == 29){
+        window.open('');
+    } else if (x == 30){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+        window.open('');
+    } else if (x == 2){
+
+    }else{
+        alert('Have a probem!')
+    }
+}
